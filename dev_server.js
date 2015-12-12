e16007ccdfe021a033b617ff504d9700dbcd719b
Jason Galea <jason@lecstor.com>
2015-12-12 23:40:31 +1000

    fix dev_server loading of pages below /
    - When the browser tries to load eg /login directly the webpack
    middleware ignores the request and the static html is served.
    - using connect-history-api-fallback we are able to modify the request
    for directory style paths so the webpack serves the app.

--- dev_server.js
+++ dev_server.js
@@ -3,12 +3,15 @@ var express = require('express');
 var webpack = require('webpack');
 var devMiddleware = require('webpack-dev-middleware');
 var hotMiddleware = require('webpack-hot-middleware');
+var history = require('connect-history-api-fallback');
 
 var config = require('config');
 var webpackConfig = require(config.get('webpack_config_file'));
 
 var app = express();
 var compiler = webpack(webpackConfig);
+
+app.use(history());
 
 app.use(devMiddleware(compiler, {
   noInfo: false,
