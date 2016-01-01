1dfb8be944741a79013737ae4b080d2b06ee9171
Jason Galea <jason@lecstor.com>
2016-01-01 23:58:26 +1000

    display status message in logout view
    - the logout view is now only displayed temporarily while the logout
    request is completed

--- src/views/logout-view.js
+++ src/views/logout-view.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-import LoginForm from '../components/login-form';
 import { logout } from '../store/modules/auth';
 
 export class LogoutView extends React.Component {
@@ -14,11 +13,8 @@ export class LogoutView extends React.Component {
   render () {
     return (
       <div className="container text-center">
-        <h1>This is the logout view!</h1>
-        <p>You can log back in..</p>
-        <div className="text-left col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 col-lg-offset-4 col-lg-4">
-          <LoginForm />
-        </div>
+        <h1>Logging out..</h1>
+        <p>You will be redirected to our site.</p>
       </div>
     );
   }
