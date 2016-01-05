6c0df6b22b609e719f1f8a91ea1a7173fa5f79c8
Jason Galea <jason@lecstor.com>
2016-01-05 23:57:41 +1000

    update app navbar

--- src/components/top-nav-app/index.js
+++ src/components/top-nav-app/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import { IndexLink } from 'react-router';
 import { LinkContainer } from 'react-router-bootstrap';
 import Navbar from 'react-bootstrap/lib/Navbar';
@@ -7,26 +7,35 @@ import NavItem from 'react-bootstrap/lib/NavItem';
 
 // Export bare component for testing
 // See: http://rackt.github.io/redux/docs/recipes/WritingTests.html
-export default class AltTopNav extends Component {
+export default class TopNavApp extends Component {
   render () {
     const styles = require('./style.scss');
+    const { user, loggingOut } = this.props;
 
     return (
       <Navbar fixedTop className={styles.navbar}>
         <Navbar.Header>
           <Navbar.Brand className={styles.brand}>
-            <IndexLink to="/alt"> React Startup Starter </IndexLink>
+            <IndexLink to="/app"> React Startup Starter App</IndexLink>
           </Navbar.Brand>
           <Navbar.Toggle />
         </Navbar.Header>
         <Navbar.Collapse>
           <Nav pullRight navbar>
-            <LinkContainer to="/alt/signup">
-              <NavItem>Sign Up</NavItem>
+            <LinkContainer to="/logout">
+              <NavItem>Log Out</NavItem>
             </LinkContainer>
           </Nav>
+          <p className={styles.loggedInMessage + ' navbar-text'}>Logged in as <strong>{user.email}</strong>.</p>
+          {loggingOut &&
+          <p className={styles.loggedInMessage + ' navbar-text'}>Logging Out..</p>}
         </Navbar.Collapse>
       </Navbar>
     );
   }
 }
+
+TopNavApp.propTypes = {
+  user: PropTypes.object,
+  loggingOut: PropTypes.bool,
+};
