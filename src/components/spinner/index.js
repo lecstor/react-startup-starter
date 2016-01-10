8df962113ecd5aeafc23931b8c8e7b7894f2b982
Jason Galea <jason@lecstor.com>
2016-01-10 15:18:31 +1000

    add spinner component

---
+++ src/components/spinner/index.js
@@ -0,0 +1,20 @@
+import React, { Component, PropTypes } from 'react';
+import Spinner from 'spin.js';
+
+// based on react-spin (https://github.com/thomasboyt/react-spin)
+// using spin.js       (http://fgnass.github.io/spin.js/)
+
+export default class Spin extends Component {
+  static propTypes = {
+    config: PropTypes.object,
+  }
+
+  componentDidMount () {
+    this.spinner = new Spinner(this.props.config);
+    this.spinner.spin(this.refs.container);
+  }
+
+  render () {
+    return (<span ref="container" />);
+  }
+}
